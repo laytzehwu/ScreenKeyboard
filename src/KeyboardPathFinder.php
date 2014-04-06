64ec0aa0827cc199d07b813decb9f5791fb2209f
laytze <tzeh.wu.lay@f-secure.com>
2014-04-06 06:42:13 -0700

    Add key to key finding path cases

--- src/KeyboardPathFinder.php
+++ src/KeyboardPathFinder.php
@@ -2,6 +2,92 @@
 require_once("ScreenKeyboard.php");
 class KeyboardPathFinder {
 
+    const LEFT=1;
+    const RIGHT=2;
+    const UP=3;
+    const DOWN=4;
+
+    /**
+     * Find path between keys
+     * @param - String - Starting key
+     * @param - String - Target key
+     * @param - Array - Seeking path
+     * @param - String - Tried keys
+     * @return - Boolean - Found?
+    */
+    protected function seekKeysPath($sKey1, $sKey2, &$aPath, $sTriedkeys = "") {
+        if(strpos($sTriedkeys, $sKey1) === false) {
+            $sTriedkeys .= $sKey1;
+        } else {
+            // Skip if it has been done
+            return false;
+        }
+
+        $bTheyAreInSameRow = ScreenKeyboard::keysInSameRow($sKey1, $sKey2);
+        if($bTheyAreInSameRow) {
+            $sLeftKey = ScreenKeyboard::keyLeft($sKey1);
+            if($sLeftKey === $sKey2) {
+                $aPath[] = self::LEFT;
+                return true;
+            }
+
+            $sRightKey = ScreenKeyboard::keyRight($sKey1);
+            if($sRightKey === $sKey2) {
+                $aPath[] = self::RIGHT;
+                return true;
+            }
+
+            $sNextKey1 = $sLeftKey;
+            $aNewPath1 = $aPath;
+            $aNewPath1[] = self::LEFT;
+            $sNextKey2 = $sRightKey;
+            $aNewPath2 = $aPath;
+            $aNewPath2[] = self::RIGHT;
+
+        } else {
+            $sTopKey = ScreenKeyboard::keyOnTop($sKey1);
+            if($sTopKey === $sKey2) {
+                $aPath[] = self::UP;
+                return true;
+            }
+
+            $sBottomKey = ScreenKeyboard::keyAbove($sKey1);
+            if($sBottomKey === $sKey2) {
+                $aPath[] = self::DOWN;
+                return true;
+            }
+            $sNextKey1 = $sTopKey;
+            $aNewPath1 = $aPath;
+            $aNewPath1[] = self::UP;
+            $sNextKey2 = $sBottomKey;
+            $aNewPath2 = $aPath;
+            $aNewPath2[] = self::DOWN;
+        }
+
+        $bFoundInPath1 = false;
+        $bFoundInPath2 = false;
+
+        if(self::seekKeysPath($sNextKey1, $sKey2, $aNewPath1, $sTriedkeys)) {
+            $bFoundInPath1 = true;
+        }
+        if(self::seekKeysPath($sNextKey2, $sKey2, $aNewPath2, $sTriedkeys)) {
+            $bFoundInPath2 = true;
+        }
+        if($bFoundInPath1 && $bFoundInPath2) {
+            if(count($aNewPath1) < count($aNewPath2)) {
+                // Take the shorter
+                $aPath = $aNewPath1;
+            } else {
+                $aPath = $aNewPath2;
+            }
+        } else if($bFoundInPath1) {
+            $aPath = $aNewPath1;
+        } else if($bFoundInPath2) {
+            $aPath = $aNewPath2;
+        }
+        return $bFoundInPath1 || $bFoundInPath2;
+    }
+
     /**
      * Search path from key1 to key2
      * @param - String - Key1
@@ -9,9 +95,16 @@ class KeyboardPathFinder {
      * @return - Array - Movement list.
     */
     public function getKeyToKeyPath($sKey1, $sKey2) {
+        $sKey1 = ScreenKeyboard::validKey($sKey1);
+        $sKey2 = ScreenKeyboard::validKey($sKey2);
         $aPath = array();
-
-        return $aPath;
+        if($sKey1 === $sKey2) {
+            return $aPath;
+        }
+        if(self::seekKeysPath($sKey1, $sKey2, $aPath)) {
+            return $aPath;
+        }
+        return null;
     }
 }
 ?>
--- tests/TestKeyboardPathFinder.php
+++ tests/TestKeyboardPathFinder.php
@@ -2,11 +2,49 @@
 class TestKeyboardPathFinder extends PHPUnit_Framework_TestCase{
 
     public function testKeyToKeyPath() {
+        // No movement cases
         $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "A");
         $this->assertEquals(count($aPath), 0);
+        $aPath = KeyboardPathFinder::getKeyToKeyPath(ScreenKeyboard::SPACEBAR, " ");
+        $this->assertEquals(count($aPath), 0);
+        $aPath = KeyboardPathFinder::getKeyToKeyPath(ScreenKeyboard::BACKSPACE, "\010");
+        $this->assertEquals(count($aPath), 0);
 
+        // Just arround starting key
         $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "B");
         $this->assertEquals(count($aPath), 1);
+        $this->assertEquals($aPath[0], KeyboardPathFinder::RIGHT);
+        $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "Z");
+        $this->assertEquals(count($aPath), 1);
+        $this->assertEquals($aPath[0], KeyboardPathFinder::LEFT);
+        $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "`");
+        $this->assertEquals(count($aPath), 1);
+        $this->assertEquals($aPath[0], KeyboardPathFinder::UP);
+        $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "a");
+        $this->assertEquals(count($aPath), 1);
+        $this->assertEquals($aPath[0], KeyboardPathFinder::DOWN);
+
+        // More then 1 step
+        $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "D");
+        $this->assertEquals(count($aPath), 3);
+        $this->assertEquals($aPath, array(KeyboardPathFinder::RIGHT, KeyboardPathFinder::RIGHT, KeyboardPathFinder::RIGHT));
+        $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "Y");
+        $this->assertEquals(count($aPath), 2);
+        $this->assertEquals($aPath, array(KeyboardPathFinder::LEFT, KeyboardPathFinder::LEFT));
+        $aPath = KeyboardPathFinder::getKeyToKeyPath("A", "b");
+        $this->assertNotNull($aPath);
+        $this->assertEquals(count($aPath), 2);
+        $this->assertEquals($aPath, array(KeyboardPathFinder::DOWN, KeyboardPathFinder::RIGHT));
+        // Special keys
+        $aPath = KeyboardPathFinder::getKeyToKeyPath("\010", "B");
+        $this->assertNotNull($aPath);
+        $this->assertEquals(count($aPath), 3);
+        $this->assertEquals($aPath, array(KeyboardPathFinder::DOWN, KeyboardPathFinder::RIGHT, KeyboardPathFinder::RIGHT));
+        $aPath = KeyboardPathFinder::getKeyToKeyPath(" ", "#");
+        $this->assertEquals($aPath, array(KeyboardPathFinder::UP));
+        $aPath = KeyboardPathFinder::getKeyToKeyPath(" ", "L");
+        $this->assertEquals(count($aPath), 4);
+        $this->assertEquals($aPath, array(KeyboardPathFinder::DOWN, KeyboardPathFinder::RIGHT, KeyboardPathFinder::RIGHT, KeyboardPathFinder::RIGHT));
     }
 
 
